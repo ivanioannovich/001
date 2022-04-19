74d6605968b53248a6ceba9a541f71f31b85bec9
ivanioannovich <55181371+ivanioannovich@users.noreply.github.com>
2022-04-19 09:41:13 +0300

    Update 001.js

--- 001.js
+++ 001.js
@@ -1,14 +1,15 @@
 function runBody() {
-document.body.innerHTML = 'kuku';
-// <textarea id="in"></textarea>
-// <form action="webapp.php" method="post">
-	// Текст: <input type="text" name="intext">
-	// <input type="submit">
-// </form>
-// <script>
-// document.getElementById("in").value=JSON.stringify(window.Telegram.WebApp);
-// document.getElementsByName("intext")[0].value=JSON.stringify(window.Telegram.WebApp);
-// </script>
-// ';
+document.body.innerHTML = '
+//kuku';
+<textarea id="in"></textarea>
+<form action="webapp.php" method="post">
+	Текст: <input type="text" name="intext">
+	<input type="submit">
+</form>
+<script>
+document.getElementById("in").value=JSON.stringify(window.Telegram.WebApp);
+document.getElementsByName("intext")[0].value=JSON.stringify(window.Telegram.WebApp);
+</script>
+';
 }
 ;
