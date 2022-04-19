4176846fcece3f34eced7eb463f41414d6498b71
ivanioannovich <55181371+ivanioannovich@users.noreply.github.com>
2022-04-19 09:22:37 +0300

    Create 001.js

---
+++ 001.js
@@ -0,0 +1,14 @@
+function runBody() {
+document.body.innerHTML = 'kuku';
+// <textarea id="in"></textarea>
+// <form action="webapp.php" method="post">
+	// Текст: <input type="text" name="intext">
+	// <input type="submit">
+// </form>
+// <script>
+// document.getElementById("in").value=JSON.stringify(window.Telegram.WebApp);
+// document.getElementsByName("intext")[0].value=JSON.stringify(window.Telegram.WebApp);
+// </script>
+// ';
+}
+;
